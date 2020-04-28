96ea7278d8c780f6ec1b6a61a1120123269aecf9
DenioD <41270280+DenioD@users.noreply.github.com>
2020-04-28 22:16:29 +0200

    refresh contactlist on HushChat tab

--- src/contactmodel.cpp
+++ src/contactmodel.cpp
@@ -3,8 +3,13 @@
 
 void ContactModel::renderContactList(QListWidget* view)
 {
+    while(view->count() > 0)
+    {
+        view->takeItem(0);
+    }
 	for(auto &c : AddressBook::getInstance()->getAllAddressLabels())
     {
     	view->addItem(c.getName());
+        
     }
 }
