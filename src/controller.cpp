e3e537b1a40be65929790c93be25490d4a9f5899
DenioD <41270280+DenioD@users.noreply.github.com>
2020-04-29 22:07:19 +0200

    do not render Headermemos

--- src/controller.cpp
+++ src/controller.cpp
@@ -880,6 +880,10 @@ void Controller::refreshTransactions()
                     if (!o["memo"].is_null()) 
                     {
                         memo = QString::fromStdString(o["memo"]);
+                        if (memo.startsWith("{"))
+                        {
+
+                        }else{
                         ChatItem item = ChatItem(
                                 datetime,
                                 address,
@@ -889,6 +893,7 @@ void Controller::refreshTransactions()
                             );
                         chatModel->addMessage(item);
                         
+                    }
                     }
                         
                     
@@ -929,6 +934,11 @@ void Controller::refreshTransactions()
                 if (!it["memo"].is_null())
                 {
                     memo = QString::fromStdString(it["memo"]);
+                    if (memo.startsWith("{"))
+                    {
+
+                    }else{
+
                     ChatItem item = ChatItem(
                                 datetime,
                                 address,
@@ -936,7 +946,7 @@ void Controller::refreshTransactions()
                                 memo
                             );
                     chatModel->addMessage(item);
-                }
+                }}
                     
 
                 items.push_back(
